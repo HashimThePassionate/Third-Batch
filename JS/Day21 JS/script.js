afd4e0faf40d9a1224bbc3562632185f01988b53
Muhammad Hashim <hashiimtahir@gmail.com>
2024-03-11 15:47:21 +0500

    objects and Loops

---
+++ JS/Day21 JS/script.js
@@ -0,0 +1,76 @@
+// let name_of_array1 = [1,2,3]
+// console.log(typeof(name_of_array1));
+// console.log(name_of_array1);
+// let name_of_array2 = []
+// name_of_array2['Name'] = 'Umair Nawaz'
+// console.log(typeof(name_of_array2));
+// console.log(name_of_array2);
+
+// let car = {
+//     'Car Name': 'Honda',
+//     'Model': 1999,
+//     'color': 'gray'
+// };
+
+// console.log(car);
+
+// car['Car Name'] = 'toyota'
+// console.log(car["Car Name"]);
+
+
+// const person = {
+//     name: ["Bob", "Smith", 'Hashim'],
+//     age: 32,
+//     bio: function () {
+//         console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+//     },
+//     introduceSelf: function () {
+//         console.log(`Hi! I'm ${this.name[0]}.`);
+//     },
+// };
+
+
+// console.log(person);
+// console.log(person.name[2]);
+
+
+// function CreateDetails(first_name, last_name, cnic_no) {
+//     return {
+//         'first_name':first_name,
+//         'last_name':last_name,
+//         'cnic_no':cnic_no,
+//         'get_full_name':function (){
+//             return `${this.first_name} ${this.last_name}`
+//         },
+//         'cnic_detail':function(){
+//             return `${this.cnic_no}`
+//         }
+//     }
+// }
+
+
+// let obj1 = CreateDetails('Muhammad','Hashim','14301-******-*')
+// for (const k in obj1) {
+//     console.log(obj1[k]);
+        
+//     }
+// console.log(obj1);
+// console.log(obj1.cnic_detail());
+// let no = 2
+// for(let i = 1;i <=10;i++)
+// {
+//     console.log(`${no} x ${i} = ${no*i}`);
+// }
+// let no = 2
+// let i = 1
+// do {
+//     console.log(`${no} x ${i} = ${no*i}`);
+//     i++
+// }
+// while(i>10)
+
+// while (i<10) {
+//     console.log(`${no} x ${i} = ${no*i}`);
+//     i++
+// }
+
